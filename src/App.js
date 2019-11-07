db05cd4f5986bf40d987a92890b6c35147b57cac
Ivana Skorupova <senajovaivana@gmail.com>
2019-11-07 09:39:24 +0100

    Lab5 - all tasks

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import PlayerComponent from "./PlayerComponent";
+
+const App = () => (
+  <h1>Minimal React</h1>
+  <PlayerComponent
+            nameOfPlayer= {}
+            numberOfPlayer = {2} />
+  <PlayerComponent numberOfPlayer = {2} />
+)
+
+export default App
